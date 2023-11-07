17d3f0f1a8d4f14cb3eba2d893b909b18fda2dc5
Dan Zauroff <daniel.zauroff@baruchmail.cuny.edu>
2023-11-07 00:07:39 -0500

    updated props and styling

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,15 +1,25 @@
 
 function ScoreCard(props) {
     return(
-        <div class="hover:drop-shadow-xl hover: p-10 m-10 inline-table shadow-md rounded-xl bg-neutral-800">
-            <p>Game Time: {props.gameTime}</p>
-            <p>Date : {props.Date}</p>
-
-            <p>Team One : {props.teamName_0}</p>
-            <p>Team One Score : {props.teamScore_0}</p>
-
-            <p>Team Two : {props.teamName_1}</p>
-            <p>Team Two Score : {props.teamScore_1}</p>
+        <div class="hover:drop-shadow-xl hover: p-10 m-10 flex shadow-md  rounded-xl bg-neutral-800 ">
+            
+            <div class="block justify-center m-4 p-3">
+                <img src={props.teamLogo_0} class="object-scale-down h-40 w-96 scale-50"></img>
+                <p class="text-center font-bold text-lg">{props.teamName_0}</p>
+                <p class="text-center font-black text-5xl p-4">{props.teamScore_0}</p>
+            </div>
+            
+            <div class="py-10 my-10 inline">
+                <p class="text-lg text-center font-bold">{props.gameTime}</p>
+                <p class="text-center">{props.Date}</p>
+            </div>
+            
+            
+            <div>
+                <img src={props.teamLogo_1} class="object-scale-down h-40 w-96 scale-50"></img>
+                <p class="text-center font-bold text-lg">{props.teamName_1}</p>
+                <p class="text-center font-black text-5xl p-4">{props.teamScore_1}</p>
+            </div>
             
 
         </div>
