3c257b08a678b7da10ee922b84977aaafa75c41a
Dan Zauroff <daniel.zauroff@baruchmail.cuny.edu>
2023-11-09 23:57:53 -0500

    multiple cards now fit in one row, clicking on team logo links to upcoming game

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,29 +1,34 @@
 
+
 function ScoreCard(props) {
-    return(
-        <div class="hover:drop-shadow-xl hover: p-10 m-10 flex shadow-md  rounded-xl bg-neutral-800 ">
+    return (
+        <div class="flex flex-row max-w-3xl justify-center items-center hover:drop-shadow-lg  p-6 m-10 shadow-md rounded-xl bg-neutral-800">
             
-            <div class="block justify-center m-4 p-3">
-                <img src={props.teamLogo_0} class="object-scale-down h-40 w-96 scale-50"></img>
-                <p class="text-center font-bold text-lg">{props.teamName_0}</p>
+            <div class="flex flex-col justify-center items-center m-4 p-3">
+                <a href={props.nextEvent_0}>
+                    <img src={props.teamLogo_0} class="object-scale-down h-40 w-96"></img>
+                </a>
+                
+                <p class="text-center font-bold text-lg pt-3">{props.teamName_0}</p>
                 <p class="text-center font-black text-5xl p-4">{props.teamScore_0}</p>
             </div>
             
-            <div class="py-10 my-10 inline">
+            <div class="flex flex-col justify-center items-center py-10 my-10">
                 <p class="text-lg text-center font-bold">{props.gameTime}</p>
                 <p class="text-center">{props.Date}</p>
             </div>
             
-            
-            <div>
-                <img src={props.teamLogo_1} class="object-scale-down h-40 w-96 scale-50"></img>
-                <p class="text-center font-bold text-lg">{props.teamName_1}</p>
+            <div class="flex flex-col justify-center items-center m-4 p-3">
+                <a href={props.nextEvent_1}>
+                    <img src={props.teamLogo_1} class="object-scale-down h-40 w-96"></img>
+                </a>
+                <p class="text-center font-bold text-lg pt-3">{props.teamName_1}</p>
                 <p class="text-center font-black text-5xl p-4">{props.teamScore_1}</p>
             </div>
             
-
         </div>
     )
 }
+
 
 export default ScoreCard
